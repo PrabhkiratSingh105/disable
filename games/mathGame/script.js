6a14e5146733b9697c9babafc062a202caa345a4
PrabhkiratSingh105 <Storytime388@gmail.com>
2022-06-06 22:37:16 +0530

    Update script.js

--- games/mathGame/script.js
+++ games/mathGame/script.js
@@ -86,7 +86,9 @@ function submit(){
         var correct_wrong = document.getElementById("correct_wrong")
         score = 0
         localStorage.setItem("score", score)
-        correct_wrong.innerHTML = "Wrong😔😔"
+        correct_wrong_answer = localStorage.getItem("correct_answer")
+        correct_wrong_answer = parseInt(correct_wrong_answer)
+        correct_wrong.innerHTML = "Wrong😔😔 " + correct_wrong_answer
         score_div_set = document.getElementById("score")
         score_div_set.innerHTML = "Score: " + score
         var ep = localStorage.getItem("ep")
